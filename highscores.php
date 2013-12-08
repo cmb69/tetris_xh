83d5800fd94aa0e2bce4a0a350b343f485ab1aaf
cmb69 <cmbecker69@gmx.de>
2013-12-08 15:36:39 +0000

    refactored PHP according to CS

--- highscores.php
+++ highscores.php
@@ -3,117 +3,140 @@
 /**
  * Highscore handling of Tetris_XH.
  *
- * Copyright (c) 2011-2013 Christoph M. Becker (see license.txt)
+ * PHP versions 4 and 5
+ *
+ * @category  CMSimple_XH
+ * @package   Tetris
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2011-2013 Christoph M. Becker <http://3-magi.net>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Tetris_XH
  */
 
-
+/*
+ * Turn off error reporting.
+ */
 error_reporting(0);
 
-
-if (!isset($_SESSION)) {
+/*
+ * Start the session.
+ */
+if (session_id() == '') {
     session_start();
 }
 
-
+/*
+ * Prevent direct access.
+ */
 if (empty($_SESSION['tetris_data_folder'])) {
     header('HTTP/1.0 403 Forbidden');
     exit;
 }
 
-
 /**
- * Returns $text without slashes.
+ * Returns a text with slashes removed, if appropriate.
  *
- * @param string $text
+ * @param string $text A text.
+ * 
  * @return string
  */
-function stsl($text) {
+function stsl($text)
+{
     return get_magic_quotes_gpc() ? stripslashes($text) : $text;
 }
-
 
 /**
  * Reads the highscores.
  *
- * @global array $highscores
  * @return bool
+ * 
+ * @global array The highscores.
  */
-function read_highscores() {
+function Tetris_readHighscores()
+{
     global $highscores;
 
-    $fn = $_SESSION['tetris_data_folder'].'highscores.dat';
-    if (($cnt = file_get_contents($fn)) === FALSE
-	    || ($highscores = unserialize($cnt)) === FALSE) {
-	$highscores = array();
+    $fn = $_SESSION['tetris_data_folder'] . 'highscores.dat';
+    if (($cnt = file_get_contents($fn)) === false
+        || ($highscores = unserialize($cnt)) === false
+    ) {
+        $highscores = array();
     }
 }
-
 
 /**
  * Writes back the highscores.
  *
- * @global array $highscores
  * @return void
+ * 
+ * @global array The highscores.
  */
-function write_highscores() {
+function Tetris_writeHighscores()
+{
     global $highscores;
 
-    $fn = $_SESSION['tetris_data_folder'].'highscores.dat';
-    if (($fh = fopen($fn, 'w')) !== FALSE) {
-	flock($fh, LOCK_EX);
-	fputs($fh, serialize($highscores));
-	flock($fh, LOCK_UN);
-	fclose($fh);
+    $fn = $_SESSION['tetris_data_folder'] . 'highscores.dat';
+    if (($fh = fopen($fn, 'w')) !== false) {
+        flock($fh, LOCK_EX);
+        fputs($fh, serialize($highscores));
+        flock($fh, LOCK_UN);
+        fclose($fh);
     }
 }
 
-
 /**
- * Enters new highscore.
+ * Enters a new highscore.
+ * 
  * Strips the highscores to at most 10 entries.
  *
- * @global array $highscores
- * @param string $name
- * @param string $score
+ * @param string $name  A player name.
+ * @param string $score A score.
+ *
+ * @return void
+ * 
+ * @global array The highscores
  */
-function enter_highscore($name, $score) {
+function Tetris_enterHighscore($name, $score)
+{
     global $highscores;
 
     $highscores[] = array($name, $score);
-    usort($highscores, create_function('$a, $b', 'return $b[1]-$a[1];'));
+    usort($highscores, create_function('$a, $b', 'return $b[1] - $a[1];'));
     array_splice($highscores, 10);
 }
 
-
 /**
- * Handling of the requests.
+ * Handles the requests.
  */
-read_highscores();
+Tetris_readHighscores();
 switch ($_REQUEST['action']) {
-    case 'required':
-	echo isset($highscores[9][1]) ? $highscores[9][1] : 0;
-	break;
-    case 'list':
-	echo '<div id="tetris-highscores">', "\n", '<table>', "\n";
-	foreach ($highscores as $highscore) {
-	    list($name, $score) = $highscore;
-	    echo '<tr><td class="name">', htmlspecialchars($name), '</td><td class="score">',
-		    htmlspecialchars($score), '</td></tr>', "\n";
-	}
-	echo '</table>', "\n", '</div>', "\n";
-	break;
-    case 'new':
-	$name = stsl($_POST['name']);
-	$score = stsl($_POST['score']);
-	if (time() > $_SESSION['tetris_timestamp'] + 10
-		&& strlen($name) <= 20
-		&& preg_match('/[0-9]{1,6}/', $score)) {
-	    enter_highscore($name, $score);
-	    write_highscores();
-	}
-	$_SESSION['tetris_timestamp'] = time();
-	break;
-    default:
+case 'required':
+    echo isset($highscores[9][1]) ? $highscores[9][1] : 0;
+    break;
+case 'list':
+    echo '<div id="tetris-highscores">', PHP_EOL, '<table>', PHP_EOL;
+    foreach ($highscores as $highscore) {
+        list($name, $score) = $highscore;
+        echo '<tr><td class="name">', htmlspecialchars($name, ENT_COMPAT, 'UTF-8'),
+            '</td><td class="score">', htmlspecialchars($score, ENT_COMPAT, 'UTF-8'),
+            '</td></tr>', PHP_EOL;
+    }
+    echo '</table>', PHP_EOL, '</div>', PHP_EOL;
+    break;
+case 'new':
+    $name = stsl($_POST['name']);
+    $score = stsl($_POST['score']);
+    if (time() > $_SESSION['tetris_timestamp'] + 10
+        && strlen($name) <= 20 // FIXME: use utf8_strlen()
+        && preg_match('/[0-9]{1,6}/', $score)
+    ) {
+        Tetris_enterHighscore($name, $score);
+        Tetris_writeHighscores();
+    }
+    $_SESSION['tetris_timestamp'] = time();
+    break;
+default:
 }
 
 ?>
